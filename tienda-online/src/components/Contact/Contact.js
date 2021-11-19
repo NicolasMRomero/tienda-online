068a5ed043bef35f1477357d4412ca0fd641e274
NicolasMRomero <nicolas.m.romero@gmail.com>
2021-11-19 16:49:19 -0300

    entrega final online shop nicolas m romero

--- tienda-online/src/components/Contact/Contact.js
+++ tienda-online/src/components/Contact/Contact.js
@@ -1,8 +1,37 @@
+import { Card, Button } from "react-bootstrap"
+import { useHistory } from 'react-router'
+import {FaGithubSquare} from 'react-icons/fa'
+import { Link } from "react-router-dom"
 
 export const Contact = () => {
+
+    const {push} = useHistory()
+
     return (
         <>
-          <h1>Contact</h1>  
+        <section>
+            <h1 className='my-3'>Contact</h1>  
+            <Card style={{ width: '18rem',margin:'auto' }}>
+            <Card.Img variant="top" src="https://firebasestorage.googleapis.com/v0/b/tienda-online-e39a3.appspot.com/o/Energica-66-2.jpg?alt=media&token=5b9abdd7-73ee-4553-a282-15d83d59e124" />
+            <Card.Body>
+                <Card.Title>Nicolás Romero</Card.Title>
+                <Card.Text>
+                This is my first React Js project.
+                You can check the source code of this site in my Github profile 👉
+                <Link to={{ pathname: "https://github.com/NicolasMRomero/tienda-online/tree/master"}} target="_blank" >
+                    <FaGithubSquare size={22} className="widget"/>
+                </Link>
+                
+                </Card.Text>
+                <Button 
+                className="btn btn-primary mx-4 my-2"
+                onClick={() => push("/")}
+                >
+                Go back
+            </Button>
+            </Card.Body>
+            </Card>
+        </section>
         </>
     )
 }
--- tienda-online/src/components/EmptyCart/EmptyCart.js
+++ tienda-online/src/components/EmptyCart/EmptyCart.js
@@ -2,6 +2,7 @@ import Button from '@restart/ui/esm/Button'
 import Lottie from 'react-lottie'
 import shopping_bag_error from './shopping-bag-error.json'
 import { useHistory } from 'react-router'
+import { Alert } from 'react-bootstrap'
 
 const EmptyCart = () => {
 
@@ -15,11 +16,14 @@ const EmptyCart = () => {
 
     return (
         <div className='empty-cart'style={{ width: '25rem', margin:'auto', padding:'2em' }}>
+             <Alert variant='warning'>
+               There are no products loaded in the cart yet
+            </Alert>
             <Lottie 
             options={defaultOptions} 
             />
             <Button 
-                className="btn btn-secondary mx-4 my-2"
+                className="btn btn-primary mx-4 my-2"
                 onClick={() => push("/")}
                 >
                 Go to home
